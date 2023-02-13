4bac386bd783a6096020094bd677d51961a9e780
aidanaerbolatova <erbolatovaaidanaa@gmail.com>
2023-02-13 13:58:15 +0600

    structs

---
+++ models/model.go
@@ -0,0 +1,96 @@
+package models
+
+import (
+	"html/template"
+	"time"
+)
+
+type User struct {
+	Id       int
+	Email    string
+	Username string
+	Password string
+}
+
+type Token struct {
+	Id        int
+	UserId    int
+	AuthToken string
+	ExpiresAT time.Time
+}
+
+type Post struct {
+	Id         int
+	UserId     int
+	Title      string
+	Text       string
+	Categories string
+	CreatedAt  string
+	Author     string
+	Like       string
+	Dislike    string
+	Image      template.URL
+}
+
+type MyPost struct {
+	User         string
+	Posts        []Post
+	Notification []Notifications
+}
+
+type PostID struct {
+	Username string
+	Post     Post
+}
+
+type CommentID struct {
+	Username string
+	Comment  Comments
+}
+
+type UserCategory struct {
+	Username string
+	Category []Post
+}
+
+type Comments struct {
+	Id        int
+	UserId    int
+	PostId    int
+	Comment   string
+	CreatedAt string
+	Author    string
+	Like      string
+	Dislike   string
+}
+
+type GetComments struct {
+	User     string
+	Post     Post
+	Comments []Comments
+	Images   []template.URL
+}
+
+type Evaluate struct {
+	Id     int
+	PostId int
+	UserId int
+	Vote   int
+}
+type Vote struct {
+	Like    string
+	Dislike string
+}
+
+type EvaluateComment struct {
+	Id        int
+	UserId    int
+	CommentId int
+	Vote      int
+}
+
+type Notifications struct {
+	From   string
+	To     string
+	Action string
+}
