b91bd612210f2f9d351903507ef99e1b61437cf8
aidanaerbolatova <erbolatovaaidanaa@gmail.com>
2023-02-13 13:39:11 +0600

    handlers

---
+++ pkg/handlers/auth.go
@@ -0,0 +1,145 @@
+package handlers
+
+import (
+	"database/sql"
+	"errors"
+	"html/template"
+	"net/http"
+	"time"
+
+	"Forum/models"
+
+	"Forum/pkg/service"
+)
+
+var (
+	TemplateSignUp = "templates/html/signUp.html"
+	TemplateSignIn = "templates/html/signIn.html"
+)
+
+func (h *Handler) SignUp(w http.ResponseWriter, r *http.Request) {
+	temp, err := template.ParseFiles(TemplateSignUp)
+	if err != nil {
+		h.HandleErrorPage(w, http.StatusInternalServerError, errors.New(http.StatusText(http.StatusInternalServerError)))
+		return
+	}
+	switch r.Method {
+	case http.MethodGet:
+		if err := temp.Execute(w, nil); err != nil {
+			h.HandleErrorPage(w, http.StatusInternalServerError, errors.New(http.StatusText(http.StatusInternalServerError)))
+			return
+		}
+	case http.MethodPost:
+		if err := r.ParseForm(); err != nil {
+			h.HandleErrorPage(w, http.StatusInternalServerError, errors.New(http.StatusText(http.StatusInternalServerError)))
+			return
+		}
+		email, ok1 := r.Form["email"]
+		username, ok2 := r.Form["username"]
+		password, ok3 := r.Form["password"]
+		if !ok1 {
+			h.HandleErrorPage(w, http.StatusBadRequest, errors.New("email is not field"))
+			return
+		} else if !ok2 {
+			h.HandleErrorPage(w, http.StatusBadRequest, errors.New("username is not field"))
+		} else if !ok3 {
+			h.HandleErrorPage(w, http.StatusBadRequest, errors.New("password is not field"))
+		}
+		user := models.User{
+			Email:    email[0],
+			Username: username[0],
+			Password: password[0],
+		}
+		err := h.services.Authorization.CreateUser(user)
+		if err != nil {
+			if errors.Is(err, service.ErrCheckInvalid) {
+				h.HandleErrorPage(w, http.StatusBadRequest, service.ErrCheckInvalid)
+				return
+			} else if errors.Is(err, sql.ErrNoRows) {
+				h.HandleErrorPage(w, http.StatusUnauthorized, errors.New(http.StatusText(http.StatusUnauthorized)))
+				return
+			}
+			h.HandleErrorPage(w, http.StatusBadRequest, errors.New(http.StatusText(http.StatusBadRequest)))
+			return
+		}
+		http.Redirect(w, r, "/signIn", http.StatusSeeOther)
+	default:
+		h.HandleErrorPage(w, http.StatusMethodNotAllowed, errors.New(http.StatusText(http.StatusMethodNotAllowed)))
+		return
+	}
+}
+
+func (h *Handler) SignIn(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodGet:
+		temp, err := template.ParseFiles(TemplateSignIn)
+		if err != nil {
+			h.HandleErrorPage(w, http.StatusInternalServerError, errors.New(http.StatusText(http.StatusInternalServerError)))
+			return
+		}
+		if err := temp.Execute(w, nil); err != nil {
+			h.HandleErrorPage(w, http.StatusInternalServerError, errors.New(http.StatusText(http.StatusInternalServerError)))
+			return
+		}
+	case http.MethodPost:
+		if err := r.ParseForm(); err != nil {
+			h.HandleErrorPage(w, http.StatusInternalServerError, errors.New(http.StatusText(http.StatusInternalServerError)))
+			return
+		}
+		username, ok1 := r.Form["username"]
+		password, ok2 := r.Form["password"]
+		if !ok1 || !ok2 {
+			h.HandleErrorPage(w, http.StatusBadRequest, errors.New(http.StatusText(http.StatusBadRequest)))
+			return
+		}
+		token, err := h.services.Authorization.GenerateToken(username[0], password[0], false)
+		if err != nil {
+			if errors.Is(err, service.ErrorWrongPassword) {
+				h.HandleErrorPage(w, http.StatusBadRequest, service.ErrorWrongPassword)
+				return
+			}
+			h.HandleErrorPage(w, http.StatusUnauthorized, errors.New(http.StatusText(http.StatusUnauthorized)))
+			return
+		}
+		http.SetCookie(w, &http.Cookie{
+			Name:  "session_token",
+			Value: token.AuthToken,
+			Path:  "/",
+		})
+		http.Redirect(w, r, "/", http.StatusSeeOther)
+	default:
+		h.HandleErrorPage(w, http.StatusMethodNotAllowed, errors.New(http.StatusText(http.StatusMethodNotAllowed)))
+		return
+	}
+}
+
+func (h *Handler) SignOut(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		if r.Method == http.MethodGet {
+			h.HandleErrorPage(w, http.StatusNotFound, errors.New(http.StatusText(http.StatusNotFound)))
+			return
+		}
+		h.HandleErrorPage(w, http.StatusMethodNotAllowed, errors.New(http.StatusText(http.StatusMethodNotAllowed)))
+		return
+	}
+	cookie, err := r.Cookie("session_token")
+	if err != nil {
+		if errors.Is(err, http.ErrNoCookie) {
+			h.HandleErrorPage(w, http.StatusUnauthorized, errors.New(http.StatusText(http.StatusUnauthorized)))
+			return
+		}
+		h.HandleErrorPage(w, http.StatusBadRequest, errors.New(http.StatusText(http.StatusBadRequest)))
+		return
+	}
+	if err := h.services.DeleteToken(cookie.Value); err != nil {
+		h.HandleErrorPage(w, http.StatusInternalServerError, errors.New(http.StatusText(http.StatusInternalServerError)))
+		return
+	}
+	http.SetCookie(w, &http.Cookie{
+		Name:    "session_token",
+		Value:   "",
+		Expires: time.Time{},
+		Path:    "/",
+	})
+	http.Redirect(w, r, "/", http.StatusSeeOther)
+}
---
+++ pkg/handlers/edit.go
@@ -0,0 +1,162 @@
+package handlers
+
+import (
+	"Forum/models"
+	"database/sql"
+	"errors"
+	"fmt"
+	"html/template"
+	"net/http"
+	"strconv"
+)
+
+var (
+	TemplateEditPost    = "templates/html/edit.html"
+	TemplateEditComment = "templates/html/editComment.html"
+)
+
+func (h *Handler) EditPost(w http.ResponseWriter, r *http.Request) {
+	temp, err := template.ParseFiles(TemplateEditPost)
+	if err != nil {
+		h.HandleErrorPage(w, http.StatusInternalServerError, err)
+		return
+	}
+	user := r.Context().Value(ctxKeyUser).(models.User)
+	result := models.PostID{}
+	result.Username = user.Username
+	switch r.Method {
+	case http.MethodGet:
+		if err := r.ParseForm(); err != nil {
+			h.HandleErrorPage(w, http.StatusInternalServerError, err)
+			return
+		}
+		postId, ok := r.Form["postID"]
+		if !ok {
+			h.HandleErrorPage(w, http.StatusBadRequest, errors.New("postID"))
+			return
+		}
+		postID, err := strconv.Atoi(postId[0])
+		if err != nil {
+			h.HandleErrorPage(w, http.StatusInternalServerError, err)
+			return
+		}
+		post, err := h.services.GetPostByPostID(postID)
+		if err != nil {
+			if errors.Is(err, sql.ErrNoRows) {
+				h.HandleErrorPage(w, http.StatusNotFound, errors.New(http.StatusText(http.StatusNotFound)))
+				return
+			}
+			h.HandleErrorPage(w, http.StatusInternalServerError, err)
+			return
+		}
+		result.Post = post
+		if err := temp.Execute(w, result); err != nil {
+			h.HandleErrorPage(w, http.StatusInternalServerError, err)
+			return
+		}
+	case http.MethodPost:
+		if err := r.ParseForm(); err != nil {
+			h.HandleErrorPage(w, http.StatusInternalServerError, errors.New(http.StatusText(http.StatusInternalServerError)))
+			return
+		}
+		title, ok1 := r.Form["title"]
+		if !ok1 {
+			h.HandleErrorPage(w, http.StatusBadRequest, errors.New(http.StatusText(http.StatusBadRequest)+" title: empty"))
+			return
+		}
+		text, ok2 := r.Form["text"]
+		if !ok2 {
+			h.HandleErrorPage(w, http.StatusBadRequest, errors.New(http.StatusText(http.StatusBadRequest)+" text: empty"))
+		}
+		postId, ok := r.Form["postId"]
+		if !ok {
+			h.HandleErrorPage(w, http.StatusBadRequest, errors.New(http.StatusText(http.StatusBadRequest)+" postId: empty"))
+			return
+		}
+		postID, err := strconv.Atoi(postId[0])
+		if err != nil {
+			h.HandleErrorPage(w, http.StatusInternalServerError, errors.New(http.StatusText(http.StatusInternalServerError)))
+			return
+		}
+		err = h.services.EditPost(postID, title[0], text[0])
+		if err != nil {
+			h.HandleErrorPage(w, http.StatusInternalServerError, errors.New(http.StatusText(http.StatusInternalServerError)))
+			return
+		}
+		http.Redirect(w, r, "/myposts", http.StatusSeeOther)
+	default:
+		h.HandleErrorPage(w, http.StatusMethodNotAllowed, errors.New(http.StatusText(http.StatusMethodNotAllowed)))
+		return
+	}
+}
+
+func (h *Handler) EditComment(w http.ResponseWriter, r *http.Request) {
+	temp, err := template.ParseFiles(TemplateEditComment)
+	if err != nil {
+		h.HandleErrorPage(w, http.StatusInternalServerError, err)
+		return
+	}
+	user := r.Context().Value(ctxKeyUser).(models.User)
+	result := models.CommentID{}
+	result.Username = user.Username
+	switch r.Method {
+	case http.MethodGet:
+		if err := r.ParseForm(); err != nil {
+			h.HandleErrorPage(w, http.StatusInternalServerError, err)
+			return
+		}
+		commentId, ok := r.Form["commentID"]
+		if !ok {
+			h.HandleErrorPage(w, http.StatusBadRequest, errors.New(http.StatusText(http.StatusBadRequest)))
+			return
+		}
+		commentID, err := strconv.Atoi(commentId[0])
+		if err != nil {
+			h.HandleErrorPage(w, http.StatusInternalServerError, err)
+			return
+		}
+		comment, err := h.services.GetCommentById(commentID)
+		if err != nil {
+			fmt.Println(err)
+			if errors.Is(err, sql.ErrNoRows) {
+				h.HandleErrorPage(w, http.StatusNotFound, errors.New(http.StatusText(http.StatusNotFound)))
+				return
+			}
+			h.HandleErrorPage(w, http.StatusInternalServerError, err)
+			return
+		}
+		result.Comment = comment
+		if err := temp.Execute(w, result); err != nil {
+			h.HandleErrorPage(w, http.StatusInternalServerError, err)
+			return
+		}
+	case http.MethodPost:
+		if err := r.ParseForm(); err != nil {
+			h.HandleErrorPage(w, http.StatusInternalServerError, errors.New(http.StatusText(http.StatusInternalServerError)))
+			return
+		}
+		text, ok2 := r.Form["comment"]
+		if !ok2 {
+			h.HandleErrorPage(w, http.StatusBadRequest, errors.New(http.StatusText(http.StatusBadRequest)+" comment: empty"))
+		}
+		commentId, ok := r.Form["commentId"]
+		if !ok {
+			h.HandleErrorPage(w, http.StatusBadRequest, errors.New(http.StatusText(http.StatusBadRequest)+" postId: empty"))
+			return
+		}
+		commentID, err := strconv.Atoi(commentId[0])
+		if err != nil {
+			h.HandleErrorPage(w, http.StatusInternalServerError, errors.New(http.StatusText(http.StatusInternalServerError)))
+			return
+		}
+		err = h.services.EditComment(commentID, text[0])
+		if err != nil {
+			h.HandleErrorPage(w, http.StatusInternalServerError, errors.New(http.StatusText(http.StatusInternalServerError)))
+			return
+		}
+		http.Redirect(w, r, "/", http.StatusSeeOther)
+	default:
+		h.HandleErrorPage(w, http.StatusMethodNotAllowed, errors.New(http.StatusText(http.StatusMethodNotAllowed)))
+		return
+	}
+}
